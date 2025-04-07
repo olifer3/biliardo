b9d0875b85b46a420a2f5c7612b9dcf00424fa7e
eliacera2003 <elia.cera@studio.unibo.it>
2025-04-07 18:08:24 +0200

    Aggiunta parte statistica con salvataggio file, dovrebbe funzionare tutto con generazione di file, che potete vedere con ls*.txt

---
+++ statistics.hpp
@@ -0,0 +1,30 @@
+#ifndef STATISTICS_HPP
+#define STATISTICS_HPP
+
+#include "billiard.hpp"
+#include "particle.hpp"
+#include <vector>
+
+
+// Risultati statistici
+struct StatsResult {
+    float mean;
+    float stddev;
+    float skewness;
+    float kurtosis;
+};
+
+void save_to_file(const std::string& filename, const std::vector<float>&data); //funzione per scrivere su testo
+
+// Funzione principale di simulazione
+void run_statistics(
+    int N,
+    float mu_y0,
+    float sigma_y0,
+    float mu_theta0_deg,
+    float sigma_theta0_deg,
+    float velocity,
+    const Billiard& billiard
+);
+
+#endif
